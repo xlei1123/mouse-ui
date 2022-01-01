e083b039e9a8470d86b602b8cec8531c41a6ff29
layxiang <layxiang@tencent.com>
2022-01-01 17:39:34 +0800

    feat: 倒计时组件

--- src/count-down/index.md
+++ src/count-down/index.md
@@ -7,7 +7,7 @@ group:
 
 ## 倒计时
 
-### Demo:
+### 简单用法:
 
 ```tsx
 import React, { useState } from 'react';
@@ -20,7 +20,29 @@ export default () => {
   };
   return (
     <TimerCountDown
-      format={{ hours: ':', minutes: ':', seconds: '' }}
+      format={{ hours: '小时', minutes: '分', seconds: '' }}
+      seconds={10}
+      onTimeUp={onTimeUp}
+    />
+  );
+};
+```
+
+### 显示天数:
+
+```tsx
+import React, { useState } from 'react';
+import { TimerCountDown } from 'mouse-ui';
+import style from './index.less';
+
+export default () => {
+  const onTimeUp = () => {
+    console.log('时间到');
+  };
+  return (
+    <TimerCountDown
+      isShowDay={true}
+      format={{ hours: '小时', minutes: '分', seconds: '' }}
       seconds={10}
       onTimeUp={onTimeUp}
     />
@@ -30,12 +52,18 @@ export default () => {
 
 ### 参数
 
-| 参数       | 说明           | 类型    | 可选值 | 默认值                                            |
-| ---------- | -------------- | ------- | ------ | ------------------------------------------------- |
-| isShowDay  | 是否显示天数   | Boolean | -      | false                                             |
-| isShowHour | 是否显示小时   | Boolean | -      | true                                              |
-| format     | 格式化分割符号 | Object  | -      | day: '天',hours: '时',minutes: '分',seconds: '秒' |
-| day        | 天数           | Number  | -      | 0                                                 |
-| hours      | 小时           | Number  | -      | 0                                                 |
-| minutes    | 分钟           | Number  | -      | 0                                                 |
-| seconds    | 秒             | Number  | -      | 0                                                 |
+| 参数          | 说明           | 类型    | 可选值 | 默认值                                            |
+| ------------- | -------------- | ------- | ------ | ------------------------------------------------- |
+| isShowDay     | 是否显示天数   | Boolean | -      | false                                             |
+| isShowHour    | 是否显示小时   | Boolean | -      | true                                              |
+| isShowMinutes | 是否显示分钟   | Boolean | -      | true                                              |
+| format        | 格式化分割符号 | Object  | -      | day: '天',hours: '时',minutes: '分',seconds: '秒' |
+| day           | 天数           | Number  | -      | 0                                                 |
+| hours         | 小时           | Number  | -      | 0                                                 |
+| minutes       | 分钟           | Number  | -      | 0                                                 |
+| seconds       | 秒             | Number  | -      | 0                                                 |
+
+### 事件
+
+| 事件名称 | 说明 | 返回参数 |
+| onTimeUp | 倒计时时间到，执行的回调函数 | - |
--- src/count-down/index.tsx
+++ src/count-down/index.tsx
@@ -1,7 +1,135 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styles from './index.less';
+interface Iprops {
+  isShowDay?: boolean;
+  isShowHour?: boolean;
+  isShowMinutes?: boolean;
+  format?: {
+    day: string;
+    hours: string;
+    minutes: string;
+    seconds: string;
+  };
+  day?: number;
+  hours?: number;
+  minutes?: number;
+  seconds?: number;
+  onTimeUp?: () => void;
+}
 
-const index = () => {
-  return <div>111111</div>;
+let timer: NodeJS.Timeout | undefined;
+
+// 将时间统一转化为秒
+const toSeconds = (
+  day: number = 0,
+  hours: number = 0,
+  minutes: number = 0,
+  seconds: number = 0,
+): number => day * 60 * 60 * 24 + hours * 60 * 60 + minutes * 60 + seconds;
+
+// 将秒数统一转化设定的格式
+const calculateTime = (propsSeconds: number, isShowDay?: boolean) => {
+  let [day, hours, minutes, seconds] = [0, 0, 0, 0];
+  if (propsSeconds > 0) {
+    day = isShowDay ? Math.floor(propsSeconds / (60 * 60 * 24)) : 0;
+    hours = Math.floor(propsSeconds / (60 * 60)) - day * 24;
+    minutes = Math.floor(propsSeconds / 60) - day * 24 * 60 - hours * 60;
+    seconds =
+      Math.floor(propsSeconds) -
+      day * 24 * 60 * 60 -
+      hours * 60 * 60 -
+      minutes * 60;
+  }
+  return {
+    day,
+    hours,
+    minutes,
+    seconds,
+  };
+};
+
+const index = (props: Iprops) => {
+  let afterToSeconds = 0;
+  const {
+    day,
+    hours,
+    minutes,
+    seconds,
+    isShowDay,
+    isShowHour = true,
+    isShowMinutes = true,
+    format,
+  } = props;
+  afterToSeconds = toSeconds(day, hours, minutes, seconds);
+  // 时间渲染
+  const [countDownTime, setCountDownTime] = useState<{
+    day: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }>();
+
+  const countDown = () => {
+    afterToSeconds--;
+    const {
+      day: _day,
+      hours: _hours,
+      minutes: _minutes,
+      seconds: _seconds,
+    } = calculateTime(afterToSeconds, isShowDay);
+
+    setCountDownTime({
+      day: _day,
+      hours: _hours,
+      minutes: _minutes,
+      seconds: _seconds,
+    });
+
+    if (afterToSeconds < 0) {
+      clearTimer();
+      props.onTimeUp && props.onTimeUp();
+      return;
+    }
+
+    timer = setTimeout(() => {
+      countDown();
+    }, 1000);
+  };
+
+  const clearTimer = () => {
+    if (timer) clearTimeout(timer);
+  };
+
+  useEffect(() => {
+    countDown();
+  }, []);
+
+  return (
+    <div className={styles[`mouse-ui-count-down`]}>
+      {isShowDay ? (
+        <span>
+          {countDownTime?.day}
+          {format?.day || '天'}
+        </span>
+      ) : null}
+      {isShowHour ? (
+        <span>
+          {countDownTime?.hours}
+          {format?.hours || '小时'}
+        </span>
+      ) : null}
+      {isShowMinutes ? (
+        <span>
+          {countDownTime?.minutes}
+          {format?.minutes || '分'}
+        </span>
+      ) : null}
+      <span>
+        {countDownTime?.seconds}
+        {format?.seconds || '秒'}
+      </span>
+    </div>
+  );
 };
 
 export default index;
